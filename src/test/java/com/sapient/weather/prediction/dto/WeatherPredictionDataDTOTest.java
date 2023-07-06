1e2e04f20101bcb29659ac767753197dee64286e
Pulkit Gupta <pulkit.gupta@gojek.com>
2023-07-06 21:05:44 +0530

    Add unit tests for DTOs

---
+++ src/test/java/com/sapient/weather/prediction/dto/WeatherPredictionDataDTOTest.java
@@ -0,0 +1,12 @@
+package com.sapient.weather.prediction.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherPredictionDataDTOTest {
+
+  @Test
+  void setAdvices() {
+  }
+}

3dcabb1dca3242045df76d6b2db94390a76de31f
Pulkit Gupta <pulkit.gupta@gojek.com>
2023-07-06 20:50:43 +0530

    Add Startup file for SpringBoot application

---
+++ src/main/java/com/sapient/weather/prediction/WeatherPredictionApplication.java
@@ -0,0 +1,21 @@
+package com.sapient.weather.prediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherPredictionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WeatherPredictionApplication.class, args);
+  }
+}
+
+/*
+
+TODO Method level documentation
+TODO README.md
+TODO Commit history (logical commits)
+TODO JACOCO & Code coverage file
+
+ */
